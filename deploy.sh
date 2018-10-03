e944c5b7d8bb613aa6ba34e89c78c54759696e38
baseem <baseem@gmail.com>
2018-10-03 11:47:09 -1000

    project update

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,22 @@ trap ctrl_c INT
 # Ask the user for their name
 echo ${YELLOW}Lets create a AWS group, what name would you like to give it${RED}?
 read groupname
-aws iam create-group --group-name $groupname
-echo ${GREEN}AWS $groupname has been created!
+
+{ # try
+
+    aws iam create-group --group-name $groupname &&
+    echo ${CYAN}AWS $groupname has been created!
+    #save your output
+
+} || 
+
 echo ${YELLOW}For the AWS group $groupname, what policy would you like to attach${RED}?
 read policy
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/$policy --group-name $groupname
-echo ${GREEN}Fantastic! the $policy has been attached to $groupname
-echo ***************************
-echo ${CYAN}Wow! That was easy...
-echo ${GREEN}Okay what is your username, we will add it to the new group${RED}?
-read username
-aws iam add-user-to-group --user-name $username --group-name $groupname
-echo ${CYAN}Done and Done!
+echo ${CYAN}Fantastic! the $policy has been attached to $groupname
+# echo ***************************
+# echo ${CYAN}Wow! That was easy...
+# echo ${GREEN}Okay what is your username, we will add it to the new group${RED}?
+# read username
+# aws iam add-user-to-group --user-name $username --group-name $groupname
+# echo ${CYAN}Done and Done!
