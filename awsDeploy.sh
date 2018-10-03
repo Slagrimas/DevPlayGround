414c5e37f9113c6642bf8cc4a2fa1f2c470324d9
ba5eem <baseem@gmail.com>
2018-10-02 16:17:15 -1000

    project update

---
+++ awsDeploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[93m"
+CYAN="\033[96m"
+BOLDUNDERLINE="\033[1;4m"
+
+function ctrl_c() {
+  echo "${RED}${BOLDUNDERLINE}**** You have exited during mid process, sorry to see you leave ****"
+  echo "${GREEN} You can start over again...just run the script"
+  exit 1
+}
+
+# listen for ctrl_c during process - call the func
+trap ctrl_c INT
+
+# Ask the user for their name
+echo ${YELLOW}Lets create a AWS group, what name would you like to give it${RED}?
+read groupname
+aws iam create-group --group-name $groupname
+echo ${GREEN}AWS $groupname has been created!
+echo ${YELLOW}For the AWS group $groupname, what policy would you like to attach${RED}?
+read policy
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/$policy --group-name $groupname
+echo ${GREEN}Fantastic! the $policy has been attached to $groupname
+echo ***************************
+echo ${CYAN}Wow! That was easy...
+echo ${GREEN}Okay what is your username, we will add it to the new group${RED}?
+read username
+aws iam add-user-to-group --user-name $username --group-name $groupname
+echo ${CYAN}Done and Done!
