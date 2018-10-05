3f58ea4ce43d1804d5866dbd46c8f1cd53185402
baseem <baseem@gmail.com>
2018-10-04 14:12:59 -1000

    project update

---
+++ crud.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#KEYS --------->
+# --silent => if successful no output required
+
+
+#COLOR VARIABLES
+RED="\033[1;31m" # warning echo
+GREEN="\033[1;32m" # info echo
+YELLOW="\033[93m" # question echo
+CYAN="\033[96m" # success echo
+BOLDUNDERLINE="\033[1;4m" # warning echo
+
+# Check bash version --silent
+if test -z "$BASH_VERSION"; then
+  echo "${RED}Please run this script using bash, not sh or any other shell." >&2
+  exit 1
+fi
+
+#FUNCTIONS --------->
+
+#TRAP CTRL-C
+function ctrl_c() {
+  echo "\n${RED}${BOLDUNDERLINE}GOOD-BYE"
+  exit 1
+}
+
+
+#FUNCTIONS --------->
+
+# TRAP CTRL-C
+trap ctrl_c INT
+# TRAP CTRL-C
+
+
+# SYSTEM VARIABLES
+re='^[0-9]+$' #regex integer check
+
+# ARGUMENT VARIABLES
+PORT=$1
+
+# Checking if argument is empty
+if [ "$PORT" == "" ]; then
+		echo "Please try again, pass in a port number ex. crud 3000" >&2;
+		exit 1
+fi
+
+# Checking if argument is an integer
+if ! [[ $PORT =~ $re ]] ; then
+   echo "Please enter a port number only - ex. crud 3000" >&2; 
+   exit 1
+fi
+
+# Checking if more than one argument was passed
+if [ "$#" -ne 1 ]; then
+    echo "Please enter only one argument - ex. crud 3000"
+fi
+
+echo 'everything worked!'
+
--- win.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-open -na 'Google Chrome' --args --new-window 'https://www.npmjs.com/'
