5620a4c6cc6ab9af31764fd5e7aa2588efea96ca
baseem <baseem@gmail.com>
2018-10-25 08:15:24 -1000

    project update

--- cico.sh
+++ cico.sh
@@ -7,6 +7,24 @@ function ctrl_c() {
 }
 trap ctrl_c INT
 
-# start editing your bash script here
 
+# TEST VARIABLES
+re='^[0-9]+$' #regex integer check
 
+# ARGUMENT VARIABLES
+user=$1
+time=$2
+
+# Checking if argument is empty
+if [ "$user" == "" ]; then
+		echo "Please try again, ex. cico baseem time" >&2;
+		exit 1
+fi
+
+# Checking if argument is empty
+if [ "$time" == "" ]; then
+		echo "Please try again, ex. cico baseem time" >&2;
+		exit 1
+fi
+
+echo $user $time 
